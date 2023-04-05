bb27d896bebaf63cc1dc3cc4222cfd36e0776020
AyushMainali123 <ayushmainali11@gmail.com>
2023-04-05 18:32:45 +0545

    feat: updated comment text

--- src/components/feature/converter-form/ConverterForm.datas.ts
+++ src/components/feature/converter-form/ConverterForm.datas.ts
@@ -13,11 +13,11 @@ export const converterDatas: IConverterDatas = {
   currency: [
     {
       name: "NEP",
-      value: 1, // It is the exchange rate in relative to standard value. Here, value 1 indicates 1 NEP = 1 standardValue
+      value: 3, // The value 3 indicates, 3 Standard Value = 1 NEP Value
     },
     {
       name: "BUSD",
-      value: 3, // It is the exchange rate in relative to standard value. Here, value 1 indicates 3 BUSD = 1 standardValue
+      value: 1, // The value 1 indicates, 1 standard value  = 1 BUSD value
     },
   ],
 };
